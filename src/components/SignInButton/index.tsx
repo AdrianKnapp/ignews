f37207b2acc1298edf297387dd3a2c8a605f0967
Adrian Knapp <adrknapp@gmail.com>
2021-10-07 22:09:15 -0300

    Github login had done

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,25 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-
+import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type="button"
-      className={styles.signInButton}>
-        <FaGithub color="#eba417"/>
-          Adrian Knapp
+      className={styles.signInButton}
+      onClick={() => signOut()}>
+        <FaGithub color="#1ece17"/>
+          {session.user.name}
         <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button
       type="button"
-      className={styles.signInButton}>
+      className={styles.signInButton}
+      onClick={() => signIn('github')}>
         <FaGithub color="#eba417"/>
         Sign In with GitHub
     </button>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,12 +5,10 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: 'read:user',
     }),
     // ...add more providers here
-  ],
-
-  // A database is optional, but required to persist accounts in a database
-  database: process.env.DATABASE_URL,
+  ]
 })
