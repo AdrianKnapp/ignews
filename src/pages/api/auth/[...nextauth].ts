2fb2fb9bfdec702444b936ff56d6c717415a7f06
Adrian Knapp <adrknapp@gmail.com>
2021-10-22 21:25:38 -0300

    FaunaDB added

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,5 +14,45 @@ export default NextAuth({
       scope: 'read:user',
     }),
     // ...add more providers here
-  ]
+  ],
+  jwt: {
+    signingKey: process.env.SIGNIN_KEY
+  },
+  callbacks: {
+    signIn: async (user, account, profile) => {
+      const { email } = user;
+
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              { data: {
+                email
+              } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 })
